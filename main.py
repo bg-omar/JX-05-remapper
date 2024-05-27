f4bfd2e8743112f482b903804c7e60ef1c1d1dd1
Omar Iskandarani <info@omariskandarani.com>
2024-05-27 23:07:26 +0200

    almost something

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 from typing import Self
 import bluetooth
+import keyboard
 from pynput.keyboard import Controller, Key
 import asyncio
 from bleak import BleakClient, BleakScanner
+
+from service_uuid import JX_05, uuids, MY_CHAR_UUID
 
 
 async def scan_ble_devices():
@@ -33,17 +36,16 @@ def scan_bluetooth_devices():
 
 
 # Bluetooth device address of your JX-05 remote ring
-device_address = "d8:d2:c7:d2:7b:ad"  # Replace with your device's MAC address BluetoothLE#BluetoothLE00:1a:7d:da:71:13-d8:d2:c7:d2:7b:ad
+device_address = JX_05  # Replace with your device's MAC address BluetoothLE#BluetoothLE00:1a:7d:da:71:13-d8:d2:c7:d2:7b:ad
 
 # Initialize the keyboard controller
-keyboard = Controller()
+keyboard_controls = Controller()
 
 
 # Function to simulate key press
 def press_key(key):
-    keyboard.press(key)
-    keyboard.release(key)
-
+    keyboard_controls.press(key)
+    keyboard_controls.release(key)
 
 
 # Key mappings dictionary
@@ -57,15 +59,6 @@ def update_mapping(button, key):
     key_mappings[button] = key
 
 
-# Function to handle notifications from the BLE device
-def notification_handler(sender, data):
-    print(f"Received data: {data}")
-    if data == b'Button1':
-        press_key(key_mappings["Button1"])
-    elif data == b'Button2':
-        press_key(key_mappings["Button2"])
-
-
 async def show_services(client):
     services = await client.get_services()
     for service in services:
@@ -73,6 +66,12 @@ async def show_services(client):
         for char in service.characteristics:
             print(f"  Characteristic: {char}")
             print(f"    Properties: {char.properties}")
+
+
+# Function to handle notifications from the BLE device
+def notification_handler(sender, data):
+    print(f"->From sender: {sender}")
+    print(f"Received data: {data}")
 
 
 async def connect_to_device(address):
@@ -89,13 +88,15 @@ async def connect_to_device(address):
             for char in service.characteristics:
                 if "notify" in char.properties:
                     characteristic_uuid = char.uuid
-                    break
+
             if characteristic_uuid:
                 break
 
         if characteristic_uuid:
             await client.start_notify(characteristic_uuid, notification_handler)
             print(f"Subscribed to notifications for characteristic: {characteristic_uuid}")
+            await client.start_notify(uuids["vendor_specific_notify"], notification_handler)
+            print(f"Subscribed to notifications for characteristic: {uuids['vendor_specific_notify']}")
 
             # Keep the connection open to receive notifications
             while True:
@@ -132,10 +133,49 @@ class KeyMapperApp(tk.Tk):
         update_mapping("Button2", getattr(Key, selected_key))
 
 
+async def handle_button_press(sender, data):
+    # Replace the button number and function number with the ones you want to map
+    button_number = 1
+    function_number = 1
+
+    # Check if the notification is from the characteristic we're interested in
+    if sender == MY_CHAR_UUID:
+        # Check if the button press is for the button we're interested in
+        if data[0] == button_number and data[1] == function_number:
+            # Simulate a key press for the "a" key
+            keyboard.press("a")
+            keyboard.release("a")
+
+
+async def main():
+    # Create a client object
+    address = JX_05
+    print(f"  ---> address:  {address}")
+    client = BleakClient(address)
+    print(f"  ---> client:  {client}")
+
+    # Connect to the device
+    await client.connect()
+
+    # Subscribe to notifications from the characteristic that sends button press notifications
+    await client.start_notify(MY_CHAR_UUID, handle_button_press)
+
+    # Wait for notifications to be received
+    while True:
+        await asyncio.sleep(1)
+
+    # Stop notifications and disconnect from the device
+    await client.stop_notify(MY_CHAR_UUID)
+    await client.disconnect()
+
+
 if __name__ == "__main__":
+    # scan_bluetooth_devices()
     asyncio.run(scan_ble_devices())
     asyncio.run(connect_to_device(device_address))
-    # scan_bluetooth_devices()
-    app = KeyMapperApp()
-    app.after(100, lambda: asyncio.run(connect_to_device(device_address)))
-    app.mainloop()
+    # app = KeyMapperApp()
+    # app.after(100, lambda: asyncio.run(connect_to_device(device_address)))
+    # app.mainloop()
+
+    asyncio.run(main())
+
---
+++ service_uuid.py
@@ -0,0 +1,85 @@
+JX_05 = "D8:D2:C7:D2:7B:AD"
+UnoR4 = "DC:54:75:C3:D9:ED"
+
+# Replace MY_CHAR_UUID with the UUID of the characteristic that sends the button press notifications
+MY_CHAR_UUID: str = "0000ae42-0000-1000-8000-00805F9B34FB"
+
+# HID: 0x1812 # PRIMARY SERVICE
+# UUID: 00002A4D-0000-1000-8000-00805F9B34FB
+# UUID: 0x2902
+# UUID: 0x2908
+#
+# Read-REPORT_MAP:                            00002A4B-0000-1000-8000-00805F9B34FB
+# ReadWriteNotify-BOOT_MOUSE_INPUT_REPORT:    00002A33-0000-1000-8000-00805F9B34FB
+# Read-HID_INFORMATION:                       00002A4A-0000-1000-8000-00805F9B34FB
+# Write-HID_CONTROL_POINT:                    00002A4C-0000-1000-8000-00805F9B34FB
+
+uuids = {
+    "peripheral_preferred_connection_parameters": "00002a04-0000-1000-8000-00805f9b34fb",
+    "generic_access_profile":                     "00001800-0000-1000-8000-00805f9b34fb",
+    "device_name":                                "00002a00-0000-1000-8000-00805f9b34fb",
+    "appearance":                                 "00002a01-0000-1000-8000-00805f9b34fb",
+    "generic_attribute_profile":                  "00001801-0000-1000-8000-00805f9b34fb",
+    "service_changed":                            "00002a05-0000-1000-8000-00805f9b34fb",
+    "device_information":                         "0000180a-0000-1000-8000-00805f9b34fb",
+    "manufacturer_name_string":                   "00002a29-0000-1000-8000-00805f9b34fb",
+    "model_number_string":                        "00002a24-0000-1000-8000-00805f9b34fb",
+    "serial_number_string":                       "00002a25-0000-1000-8000-00805f9b34fb",
+    "hardware_revision_string":                   "00002a27-0000-1000-8000-00805f9b34fb",
+    "firmware_revision_string":                   "00002a26-0000-1000-8000-00805f9b34fb",
+    "software_revision_string":                   "00002a28-0000-1000-8000-00805f9b34fb",
+    "system_id":                                  "00002a23-0000-1000-8000-00805f9b34fb",
+    "regulatory_cert_data_list":                  "00002a2a-0000-1000-8000-00805f9b34fb",
+    "pnp_id":                                     "00002a50-0000-1000-8000-00805f9b34fb",
+    "battery_service":                            "0000180f-0000-1000-8000-00805f9b34fb",
+    "battery_level":                              "00002a19-0000-1000-8000-00805f9b34fb",
+    "vendor_specific":                            "0000ae40-0000-1000-8000-00805f9b34fb",
+    "vendor_specific_write":                      "0000ae41-0000-1000-8000-00805f9b34fb",
+    "vendor_specific_notify":                     "0000ae42-0000-1000-8000-00805f9b34fb"
+}
+
+handles = {
+  "generic_access_profile": 1,
+  "device_name": 2,
+  "appearance": 4,
+  "peripheral_preferred_connection_parameters": 6,
+  "generic_attribute_profile": 8,
+  "service_changed": 9,
+  "device_information": 12,
+  "manufacturer_name_string": 13,
+  "model_number_string": 15,
+  "serial_number_string": 17,
+  "hardware_revision_string": 19,
+  "firmware_revision_string": 21,
+  "software_revision_string": 23,
+  "system_id": 25,
+  "regulatory_cert_data_list": 27,
+  "pnp_id": 29,
+  "battery_service": 31,
+  "battery_level": 32,
+  "vendor_specific": 74,
+  "vendor_specific_write": 75,
+  "vendor_specific_notify": 77
+}
+
+
+properties = {
+    "device_name": ["read"],
+    "appearance": ["read"],
+    "peripheral_preferred_connection_parameters": ["read"],
+    "service_changed": ["indicate"],
+    "manufacturer_name_string": ["read"],
+    "model_number_string": ["read"],
+    "serial_number_string": ["read"],
+    "hardware_revision_string": ["read"],
+    "firmware_revision_string": ["read"],
+    "software_revision_string": ["read"],
+    "system_id": ["read"],
+    "regulatory_cert_data_list": ["read"],
+    "pnp_id": ["read"],
+    "battery_level": ["read", "notify"],
+    "vendor_specific_write": ["write-without-response"],
+    "vendor_specific_notify": ["notify"]
+}
+
+
